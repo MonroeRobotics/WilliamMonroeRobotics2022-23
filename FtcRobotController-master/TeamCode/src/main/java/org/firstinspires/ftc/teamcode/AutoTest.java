f0013da551f23fa73890fa25e3c27a3b50cf1944
RobotNatchos <robot6895games@gmail.com>
2022-11-23 22:30:00 -0500

    Added comments to auto code

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -24,12 +24,15 @@ public class AutoTest extends OpMode {
 
     @Override
     public void init(){
+        //gets webcam name and Id from configfile and initializes OpenCvWebcam object "webcam"
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
 
+        //sets webcam Computer Vision Pipeline to examplePipeline
         webcam.setPipeline(new examplePipeline());
 
+        //Starts streaming camera
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -50,45 +53,51 @@ public class AutoTest extends OpMode {
 
     class examplePipeline extends OpenCvPipeline {
 
+        //Initializing Variables
         Mat HSV = new Mat();
+        Mat outPut = new Mat();
         Mat leftCrop;
         Mat rightCrop;
         Mat centerCrop;
         double leftavgfin;
         double rightavgfin;
         double centeravgfin;
-        Mat outPut = new Mat();
         Scalar rectColor1 = new Scalar(255.0, 0.0, 0.0);
         Scalar rectColor2 = new Scalar(255.0, 255.0, 0.0);
         Scalar rectColor3 = new Scalar(255.0, 0.0, 255.0);
 
+        //Loops and processes every frame and returns desired changed
         public Mat processFrame(Mat input) {
 
+            //changes Mat "input" from RGB to HSV and saves under MAT HSV
             Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
             telemetry.addLine("pipeline running");
 
+            //Creates New rectangle objects for 3 regions, Left, Right, and Center
             Rect leftRect = new Rect(250, 140, 19, 79);
             Rect rightRect = new Rect(370, 140, 19, 79);
             Rect centerRect = new Rect(270, 140, 99, 79);
 
-
-
+            //Creates the upper and lower range for the accepted HSV values for color of pole
             Scalar lowHSV = new Scalar(17,50,50);
             Scalar highHSV = new Scalar(22,255,255);
-            Mat thresh = new Mat();
 
+            //Returns Output Mat "thresh" that only contains pixels that are within low and high boundaries (lowHSV, highHSV)
+            Mat thresh = new Mat();
             Core.inRange(HSV, lowHSV, highHSV, thresh);
 
+            //creates submats(subsections) of MAT HSV using the rectangle regions
             leftCrop = HSV.submat(leftRect);
             rightCrop = HSV.submat(rightRect);
             centerCrop = HSV.submat(centerRect);
 
-
+            //copies thresh Mat to outPut and draws rectangles regions on the image for user to see
             thresh.copyTo(outPut);
             Imgproc.rectangle(outPut, leftRect, rectColor1, 2);
             Imgproc.rectangle(outPut, rightRect, rectColor2, 2);
             Imgproc.rectangle(outPut, centerRect, rectColor3, 2);
 
+            //Returns to display outPut Mat
             return outPut;
         }
     }
