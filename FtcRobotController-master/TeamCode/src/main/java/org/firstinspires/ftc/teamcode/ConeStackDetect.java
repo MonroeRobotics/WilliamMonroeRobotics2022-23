5d83d57068a0ef859c2a5b74552665335ec72b2d
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2022-12-05 19:32:26 -0500

    Cone Stack Pipeline

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConeStackDetect.java
@@ -0,0 +1,200 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import static org.opencv.imgproc.Imgproc.MORPH_OPEN;
+import static org.opencv.imgproc.Imgproc.MORPH_RECT;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Autonomous(name = "Cone Stack Detect Test", group = "Main")
+
+public class ConeStackDetect extends OpMode {
+
+
+
+    OpenCvWebcam webcam = null;
+
+
+
+    int leftLowBound = 240;
+    int leftTarget = 260;
+    int rightTarget = 390;
+    int rightHighBound = 410;
+
+    double xBounding = 0;
+    double yBounding = 0;
+
+    // based on time and power which determines turn direction and turn speed
+    @Override
+    public void init() {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcamfront");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        //sets webcam Computer Vision Pipeline to examplePipeline
+        webcam.setPipeline(new ConeStackDetect.coneDetect());
+
+        //Starts streaming camera
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+    }
+
+    public void loop(){
+
+    }
+
+    class coneDetect extends OpenCvPipeline {
+
+        //Initializing Variables
+        Mat HSV = new Mat();
+        Mat outPut = new Mat();
+        Mat thresh = new Mat();
+        Mat dilate = new Mat();
+        Mat hierarchy = new Mat();
+
+        Scalar rectColor1 = new Scalar(50, 255, 255);
+
+        //Loops and processes every frame and returns desired changed
+        public Mat processFrame(Mat input) {
+
+            //changes Mat input from RGB to HSV and saves to Mat HSV
+            Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
+
+            //Creates New rectangle objects for 3 regions, Left, Right, and Center
+            Rect leftRect = new Rect(leftTarget, 140, 1, 79);
+            Rect rightRect = new Rect(rightTarget, 140, 1, 79);
+            Rect leftBound = new Rect(leftLowBound, 140, 1, 79);
+            Rect rightBound = new Rect(rightHighBound, 140, 1, 79);
+
+            //Creates the upper and lower range for the accepted HSV values for color of pole
+            Scalar lowHSV = new Scalar(172,80,80);
+            Scalar highHSV = new Scalar(184,255,255);
+
+            //Returns Output Mat "thresh" that only contains pixels that are within low and high boundaries (lowHSV, highHSV)
+
+            Core.inRange(HSV, lowHSV, highHSV, thresh);
+
+
+            Imgproc.morphologyEx(thresh, dilate, MORPH_OPEN, Imgproc.getStructuringElement(MORPH_RECT, new Size(3, 3)));
+
+            List<MatOfPoint> contours = new ArrayList<>();
+
+            Imgproc.findContours(dilate, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            dilate.copyTo(outPut);
+
+
+            if (contours.size() != 0) {
+                MatOfPoint2f[] contoursPoly  = new MatOfPoint2f[contours.size()];
+                Rect[] boundRect = new Rect[contours.size()];
+
+                for (int i = 0; i < contours.size(); i++) {
+                    contoursPoly[i] = new MatOfPoint2f();
+                    Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
+                    boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
+                }
+
+                double maxVal = 0;
+                int maxValIdx = 0;
+                for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
+                    double contourArea = Imgproc.contourArea(contours.get(contourIdx));
+                    if (maxVal < contourArea) {
+                        maxVal = contourArea;
+                        maxValIdx = contourIdx;
+                    }
+                }
+                xBounding = boundRect[maxValIdx].x;
+
+                yBounding = boundRect[maxValIdx].x + boundRect[maxValIdx].width;
+
+                telemetry.addData("MaxContor",maxVal);
+                Imgproc.rectangle(outPut, boundRect[maxValIdx], new Scalar(150, 80, 100), 3);
+            }
+
+
+            telemetry.addData("x bounding", xBounding);
+            telemetry.addData("y bounding", yBounding);
+
+
+            //copies thresh Mat to outPut and draws rectangles regions on the image for user to see
+
+            Imgproc.rectangle(outPut, leftRect, rectColor1, 2);
+            Imgproc.rectangle(outPut, rightRect, rectColor1, 2);
+            Imgproc.rectangle(outPut, leftBound, rectColor1, 2);
+            Imgproc.rectangle(outPut, rightBound, rectColor1, 2);
+
+
+            //Returns to display outPut Mat
+            return outPut;
+        }
+    }
+}
