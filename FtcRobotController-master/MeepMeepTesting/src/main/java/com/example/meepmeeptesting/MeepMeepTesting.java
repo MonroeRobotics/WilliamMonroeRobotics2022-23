243bc63b808e8cafdb86b3c5a952ec007d8c6262
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2022-12-19 16:59:49 -0500

    Update to 8.1.1

--- FtcRobotController-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ FtcRobotController-master/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/magnetTEst.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class magnetTEst extends LinearOpMode {
+    // Define variables for our touch sensor
+    TouchSensor limit;
+
+    @Override
+    public void runOpMode() {
+        // Get the touch sensor and motor from hardwareMap
+        limit = hardwareMap.get(TouchSensor.class, "limit");
+
+        // Wait for the play button to be pressed
+        waitForStart();
+
+        // Loop while the Op Mode is running
+        while (opModeIsActive()) {
+            // If the Magnetic Limit Switch is pressed, stop the motor
+            if (limit.isPressed()) {
+                telemetry.addLine("isPressed");
+            } else { // Otherwise, run the motor
+                telemetry.addLine("notPressed");
+            }
+            telemetry.update();
+        }
+    }
+}
