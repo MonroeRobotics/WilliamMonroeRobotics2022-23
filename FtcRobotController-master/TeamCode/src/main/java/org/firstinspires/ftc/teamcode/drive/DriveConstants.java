c724bd24e758c940511d13458cb216b38f75069f
RobotNatchos <47341210+RobotNatchos@users.noreply.github.com>
2023-01-29 18:02:05 -0500

    Odo Add Testing

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,8 +34,8 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(20, 0, 7, 12.4);
+    public static final boolean RUN_USING_ENCODER = false;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 0);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.4763; // in
     public static double GEAR_RATIO = 1.06327645; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.95; // in
+    public static double TRACK_WIDTH = 15.45; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.021;
+    public static double kA = 0.0035;
+    public static double kStatic = 0.002;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -89,8 +89,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 41.5828;
     public static double MAX_ACCEL = 41.5828;
-    public static double MAX_ANG_VEL = 0.61333;
-    public static double MAX_ANG_ACCEL = 0.61333;
+    public static double MAX_ANG_VEL = 3.7630;
+    public static double MAX_ANG_ACCEL = 3.7630;
 
 
     public static double encoderTicksToInches(double ticks) {
